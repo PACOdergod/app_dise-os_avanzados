2737acd62e1382fcd018fa82aff4cb723111384c
paco <paco.33353@gmail.com>
2021-03-12 19:42:12 -0600

    Los svg ya aparecen con un slide pero faltan los puntos

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -2,13 +2,41 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: SvgPicture.asset('assets/svgs/1.svg')
-      // child: Text("hola mundo"),
-    ),);
+    return Scaffold(
+      body: Center(child: _Slides()),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide('assets/svgs/1.svg'),
+          _Slide('assets/svgs/2.svg'),
+          _Slide('assets/svgs/3.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(20.0),
+      child: SvgPicture.asset(svg));
   }
 }
