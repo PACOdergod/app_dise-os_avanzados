994094a03a8266cf544e9e443db30c69af831c55
paco <paco.33353@gmail.com>
2021-03-12 19:33:48 -0600

    ya muestra los svg en pantalla

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/flutter_svg.dart';
+
+
 class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('hola mundo'),),);
+    return Scaffold(body: Center(
+      child: SvgPicture.asset('assets/svgs/1.svg')
+      // child: Text("hola mundo"),
+    ),);
   }
 }
