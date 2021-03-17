c8fc7046a0f010344a6300e5e2e5dc8a02d0895b
paco <paco.33353@gmail.com>
2021-03-16 20:22:47 -0600

    El slideshow ya es un widget independiente al que se le puede enviar las imagenes que va a renderizar

--- lib/src/pages/sliceshow_page.dart
+++ lib/src/pages/sliceshow_page.dart
@@ -1,11 +1,18 @@
 import 'package:circular_progress/src/widgets/slideshow.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Slideshow(),),
+      body: Center(child: Slideshow(slides: [
+        SvgPicture.asset('assets/svgs/1.svg'),
+        SvgPicture.asset('assets/svgs/2.svg'),
+        SvgPicture.asset('assets/svgs/3.svg'),
+        SvgPicture.asset('assets/svgs/4.svg'),
+        SvgPicture.asset('assets/svgs/5.svg'),
+      ],),),
     );
   }
 }
--- lib/src/widgets/slideshow.dart
+++ lib/src/widgets/slideshow.dart
@@ -1,29 +1,36 @@
 import 'package:circular_progress/src/models/slider_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 
 class Slideshow extends StatelessWidget {
+  final List<Widget> slides;
+
+  Slideshow({@required this.slides});
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => SliderModel(),
       child: Center(
           child: Column(
-        children: [Expanded(child: _Slides()), _Dots()],
+        children: [Expanded(child: _Slides(this.slides)), _Dots(this.slides.length)],
       )),
     );
   }
 }
 
 class _Dots extends StatelessWidget {
+  final int cantidad;
+
+  _Dots(this.cantidad);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: 100,
       child: Row(
-        children: [_Punto(0), _Punto(1), _Punto(2)],
+        children: List.generate(this.cantidad, (index) => _Punto(index)),
         mainAxisAlignment: MainAxisAlignment.center,
       ),
     );
@@ -54,6 +61,10 @@ class _Punto extends StatelessWidget {
 }
 
 class _Slides extends StatefulWidget {
+  final List<Widget> slides;
+
+  _Slides(this.slides);
+
   @override
   __SlidesState createState() => __SlidesState();
 }
@@ -70,6 +81,7 @@ class __SlidesState extends State<_Slides> {
       // cuando esta en initState el listen debe ser false
       Provider.of<SliderModel>(context, listen: false).currentPage =
           pageViewController.page;
+
     });
   }
 
@@ -81,23 +93,19 @@ class __SlidesState extends State<_Slides> {
 
   @override
   Widget build(BuildContext context) {
+    // print("el tamaño es:${widget.slides.length}");
     return Container(
       child: PageView(
-        controller: pageViewController,
-        children: [
-          _Slide('assets/svgs/1.svg'),
-          _Slide('assets/svgs/2.svg'),
-          _Slide('assets/svgs/3.svg'),
-        ],
-      ),
+          controller: pageViewController,
+          children: widget.slides.map((e) => _Slide(e)).toList()),
     );
   }
 }
 
 class _Slide extends StatelessWidget {
-  final String svg;
+  final Widget slide;
 
-  _Slide(this.svg);
+  _Slide(this.slide);
 
   @override
   Widget build(BuildContext context) {
@@ -105,6 +113,6 @@ class _Slide extends StatelessWidget {
         width: double.infinity,
         height: double.infinity,
         padding: EdgeInsets.all(20.0),
-        child: SvgPicture.asset(svg));
+        child: slide);
   }
 }
