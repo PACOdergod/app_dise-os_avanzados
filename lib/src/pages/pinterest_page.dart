e0c6a822b9a0d38c899a711c91045da9c2930908
paco <paco.33353@gmail.com>
2021-05-23 12:58:23 -0500

    El menu ya desaparece cuando hace scroll

--- lib/src/pages/pinterest_page.dart
+++ lib/src/pages/pinterest_page.dart
@@ -2,29 +2,78 @@ import 'package:circular_progress/src/widgets/pinterest_menu.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:provider/provider.dart';
 
 class PinterstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        PinterestGrid(),
-        Positioned(
-          bottom: 20,
-          width: MediaQuery.of(context).size.width,
-          child: Center(child: PinterestMenu()),
-        )
-      ],)
+      body: ChangeNotifierProvider(
+        create: (BuildContext context) => new _MenuModel(),
+        child: Stack(
+          children: [PinterestGrid(), _MenuLocation()],
+        ),
+    ));
+  }
+}
+
+class _MenuLocation extends StatefulWidget {
+  @override
+  __MenuLocationState createState() => __MenuLocationState();
+}
+
+class __MenuLocationState extends State<_MenuLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 20,
+      width: MediaQuery.of(context).size.width,
+      child: Center(
+          child: AnimatedOpacity(
+              duration: Duration(milliseconds: 200),
+              opacity: Provider.of<_MenuModel>(context).mostrar ? 1 : 0,
+              child: PinterestMenu())),
     );
   }
 }
 
-class PinterestGrid extends StatelessWidget {
+class PinterestGrid extends StatefulWidget {
+  @override
+  _PinterestGridState createState() => _PinterestGridState();
+}
+
+class _PinterestGridState extends State<PinterestGrid> {
   final List<int> items = List.generate(100, (index) => index);
+  var controller = new ScrollController();
+  double scrollAnt = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() {
+      if (controller.offset > scrollAnt) {
+        // ha hecho un scroll hacia abajo
+        // debe ocultar el menu
+        Provider.of<_MenuModel>(context, listen: false).mostrar = false;
+      } else {
+        // mostrar menu
+        Provider.of<_MenuModel>(context, listen: false).mostrar = true;
+      }
+
+      scrollAnt = controller.offset;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new StaggeredGridView.countBuilder(
+      controller: controller,
       crossAxisCount: 4,
       itemCount: items.length,
       itemBuilder: (BuildContext context, int index) => _PinterestItem(index),
@@ -42,16 +91,27 @@ class _PinterestItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      decoration: BoxDecoration(
-        color: Colors.green,
-        borderRadius: BorderRadius.all(Radius.circular(10))
-      ),
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.all(Radius.circular(10))
+        ),
         child: new Center(
           child: new CircleAvatar(
             backgroundColor: Colors.white,
             child: new Text('$index'),
           ),
         ));
+  }
+}
+
+
+class _MenuModel with ChangeNotifier {
+  bool _mostrar = true;
+
+  bool get mostrar => _mostrar;
+  set mostrar(bool condition) {
+    _mostrar = condition;
+    notifyListeners();
   }
 }
